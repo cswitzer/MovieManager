4c8e7c6f72e6c2d25b636218556484f4b468ab13
Chase <chasehillig@yahoo.com>
2022-05-24 12:27:57 -0600

    added movie class and scaffolding

---
+++ MovieManager/Data/MovieManagerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieManager.Models;
+
+namespace MovieManager.Data
+{
+    public class MovieManagerContext : DbContext
+    {
+        public MovieManagerContext (DbContextOptions<MovieManagerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieManager.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ MovieManager/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieManager.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; } // primary key
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)] // user is not forced to enter time info and the time is ommitted
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
