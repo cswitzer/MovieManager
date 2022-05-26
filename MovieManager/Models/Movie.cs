876deccfaa2ef2e460ab180ba811f679a8f34661
Chase <chasehillig@yahoo.com>
2022-05-26 17:47:55 -0600

    added filtering

--- MovieManager/Models/Movie.cs
+++ MovieManager/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MovieManager.Models
 {
@@ -8,9 +9,11 @@ namespace MovieManager.Models
         public int ID { get; set; } // primary key
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)] // user is not forced to enter time info and the time is ommitted
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
 
     }
--- MovieManager/Pages/Movies/Index.cshtml.cs
+++ MovieManager/Pages/Movies/Index.cshtml.cs
@@ -1,9 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MovieManager.Data;
 using MovieManager.Models;
@@ -12,18 +13,36 @@ namespace MovieManager.Pages.Movies
 {
     public class IndexModel : PageModel
     {
-        private readonly MovieManager.Data.MovieManagerContext _context;
+        private readonly MovieManagerContext _context;
 
-        public IndexModel(MovieManager.Data.MovieManagerContext context)
+        public IndexModel(MovieManagerContext context)
         {
             _context = context;
         }
 
         public IList<Movie> Movie { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
 
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            // retrieves all genres in the database
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            // retrieve all movies from the database
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!string.IsNullOrEmpty(SearchString)) movies = movies.Where(s => s.Title.Contains(SearchString));
+            if (!string.IsNullOrEmpty(MovieGenre)) movies = movies.Where(x => x.Genre == MovieGenre);
+
+            Movie = await movies.ToListAsync();
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
         }
     }
 }
