4960433f4588f61f22d3cfd47d20ab740393ad43
Chase <chasehillig@yahoo.com>
2022-05-27 11:16:54 -0600

    finished tutorial

--- MovieManager/Models/Movie.cs
+++ MovieManager/Models/Movie.cs
@@ -7,14 +7,21 @@ namespace MovieManager.Models
     public class Movie
     {
         public int ID { get; set; } // primary key
+
+        [StringLength(60, MinimumLength = 3), Required]
         public string Title { get; set; }
 
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)] // user is not forced to enter time info and the time is ommitted
+        [Display(Name = "Release Date"), DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
         public string Genre { get; set; }
-        [Column(TypeName = "decimal(18, 2)")]
+
+        [Range(1, 100), DataType(DataType.Currency), Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5), Required]
+        public string Rating { get; set; } = string.Empty;
 
     }
 }
